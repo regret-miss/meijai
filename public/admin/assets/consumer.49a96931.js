f61b9ea96873df863f99217bde8759f3d10f5c20
cc <15367768958@163.com>
2026-08-14 23:46:07 +0800

    chore: update build artifacts

---
+++ public/admin/assets/consumer.49a96931.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index.d0f6ce5d.js";function u(r){return t.get({url:"/user/list",params:r})}function s(r){return t.get({url:"/user/detail",params:r})}function i(r){return t.post({url:"/user/edit",params:r})}export{u as a,s as g,i as u};
